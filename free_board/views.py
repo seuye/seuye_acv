9e7493dfbce6b4290e3007c06deb5e55bc9e5d94
unknown <realexplorer2020@gmail.com>
2021-11-02 14:58:52 +0900

    first_commit

--- free_board/views.py
+++ free_board/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from free_board.models import Post
 
 # Create your views here.
+
+def free_write(request) :
+    if request.method == 'GET':
+        return remder(request, 'write.html')
+    elif request.method == 'POST':
+     new_post = Post()
+     new_post.title = request.POST["title"]
+     new_post.content = request.POST['content']
+     new_post.save()
+     return HttpResponseRedirect("free_board/home")
