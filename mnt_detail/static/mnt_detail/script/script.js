3bd9157e0311e6812d5ce9bba16d48d2b0dfcf36
seuye <yesli0813@likelion.org>
2021-11-10 16:30:16 +0900

    map success

--- mnt_detail/static/mnt_detail/script/script.js
+++
@@ -1,2 +0,0 @@
-var array 
-document.write('text')
--- mnt_detail/views.py
+++ mnt_detail/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect,HttpResponse
 from django.urls import reverse
 import requests
 import json
+from django.http import JsonResponse  
 
 
 # Create your views here.
@@ -15,3 +16,17 @@ def mnt_detail(request):
         "contents": json_dict, 
     }
     return render(request, "mnt_detail/mnt_detail.html", context)
+
+def admin_gpx_code(request):
+    query = request.GET.get("query")
+    #query=parse.quote(query)
+    url="http://34.64.157.240:8000/admin/call/mnt_code/?gpx_code="+query
+    json_string=requests.get(url)
+    return JsonResponse(json_string.json(),safe=False)
+
+def admin_base_code(request):
+    query = request.GET.get("query")
+    #query=parse.quote(query)
+    url="http://34.64.157.240:8000/admin/call/mnt_code/?mnt_code="+query
+    json_string=requests.get(url)
+    return JsonResponse(json_string.json(),safe=False)
