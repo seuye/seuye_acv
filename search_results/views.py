344bfccf4d60b1a562cfab52fb847eccda7edf51
seuye <yesli0813@likelion.org>
2021-11-17 02:04:41 +0900

    set search

--- search_results/views.py
+++ search_results/views.py
@@ -8,7 +8,7 @@ import requests
 #     return render(request, "search_results/search_results.html")
 
 
-def search_reseults(request):
+def search_results(request):
     query = request.GET.get("query")
     url="http://34.64.157.240:8000/search/?query="+query+"&page=1&limits=10"
     json_rq = requests.get(url)
